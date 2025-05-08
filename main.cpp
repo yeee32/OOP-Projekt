2b645ccca35a0e1fa4b35f4cb9e079b40bfa7d20
Vojtěch Ožana <97116766+yeee32@users.noreply.github.com>
2025-05-08 18:54:28 +0200

    just the idea

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Packet{
+    private:
+        int id;
+        float weight;
+        float width;
+        float height;
+        Sender* sender;
+        Receiver* receiver;
+    public:
+        
+};
+
+class Package : public Packet{
+
+};
+
+class {
+
+};
+
+class Letter : public Packet{
+
+};
+
+class Sender{
+
+};
+
+class Receiver{
+
+};
+
+class PostalOffice{
+
+};
+
+// neco jako z-box od zásilkovny
+class PackageBox{
+
+}
+
+class Depo{
+
+};
+
+// Neco jako zásilkovna: postovni system
+int main(){
+
+    return 0;
+}
